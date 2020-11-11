7ae943c85f85a82da892e650ef9da486135854b4
curzes <markuspersson22@gmail.com>
2020-11-11 16:28:32 +0100

    Patches added

--- config.def.h
+++ config.def.h
@@ -1,24 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Source Code Pro:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#999999", "#000000" },
+	[SchemeSel] = { "#FFFFFF", "#999999" },
+	[SchemeSelHighlight] = { "#000000", "#FF000" },
+	[SchemeNormHighlight] = { "#00FF00", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 24; /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words

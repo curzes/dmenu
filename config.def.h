658e08509b3a687ad9b0ff1358fdc4b7414ea5a4
curzes <markuspersson22@gmail.com>
2020-11-16 12:38:30 +0100

    Adding hpitems to config.def.h

--- config.def.h
+++ config.def.h
@@ -27,6 +27,8 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 0;
 static unsigned int lineheight = 24; /* -h option; minimum height of a menu line     */
 
+/* static unsigned int hpitems = "brave,keepass,firefox,nautilus"; */
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
